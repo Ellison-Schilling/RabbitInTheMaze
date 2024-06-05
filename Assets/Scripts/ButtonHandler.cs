0310222bd1c7dc895ac82c436286ca992feba686
Ellison Largent/Schilling <ellisons@uoregon.edu>
2024-06-05 11:28:54 -0700

    Fixed some things

--- Assets/Scripts/ButtonHandler.cs
+++ Assets/Scripts/ButtonHandler.cs
@@ -1,15 +1,13 @@
 using UnityEngine;
-using System.Threading.Tasks;
 using UnityEngine.SceneManagement; // Required for SceneManager
-
 
 public class TutorialButtonHandler : MonoBehaviour
 {
     public string sceneName;
-    public async void StartGame()
+
+    public void StartGame()
     {
         // Load the given scene
-        await Task.Delay(500);
         PlayerPrefs.SetInt(sceneName, 1);
         PlayerPrefs.Save();
         SceneManager.LoadScene(sceneName);
